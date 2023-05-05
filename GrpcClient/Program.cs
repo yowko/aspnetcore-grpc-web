2a0a2a1894a20bf01fd03c04567926ebf7790190
Yowko Tsai <yowko.tsai@skylinetw.com>
2023-05-05 11:47:54 +0800

    repo init

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Greet;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5226", new GrpcChannelOptions
+{
+    HttpHandler = new GrpcWebHandler(new HttpClientHandler())
+});
+
+
+var client = new  Greeter.GreeterClient(channel);
+
+
+Console.WriteLine($"{ (await client.SayHelloAsync(new HelloRequest(){Name = "gRPCCall"})).Message}");
